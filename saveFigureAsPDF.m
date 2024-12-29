fc8a8c4563e84d2a8fa3600f0b858926d6914635
Piotr Blaszyk <piotrblaszyk@macbookpro.home>
2024-12-29 22:12:32 +0100

    folder cleanup

--- saveFigureAsPDF.m
+++ saveFigureAsPDF.m
@@ -1,9 +1,11 @@
 function saveFigureAsPDF(figHandle, filename)
-    % Apply specific settings for the given figure
-    set(figHandle, 'PaperUnits', 'inches');
-    set(figHandle, 'PaperSize', [13.33, 7.5]); % Standard PowerPoint size
-    set(figHandle, 'PaperPosition', [0, 0, 13.33, 7.5]);
-    
-    % Save the figure as a PDF
-    saveas(figHandle, filename);
+    if false
+        % Apply specific settings for the given figure
+        set(figHandle, 'PaperUnits', 'inches');
+        set(figHandle, 'PaperSize', [13.33, 7.5]); % Standard PowerPoint size
+        set(figHandle, 'PaperPosition', [0, 0, 13.33, 7.5]);
+        
+        % Save the figure as a PDF
+        saveas(figHandle, filename);
+    end
 end
